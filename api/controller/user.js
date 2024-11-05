34c93f37b4cd702078c666861568a396e6870648
dimas1509 <mdimasdimas0915@gmail.com>
2024-11-05 11:52:13 +0700

    paw

--- api/controller/user.js
+++ api/controller/user.js
@@ -1,42 +1,49 @@
 const User = require("../model/user");
 const bcrypt = require("bcrypt");
 
-const signUp = (req, res)=>{
+const signUp= (req, res)=>{
 
     bcrypt.hash(req.body.password,10)
-    .then((hash)=> {
-    const user = new User({
-        email : req.body.email,
-        password : hash 
-    });
-    user.save()
-    .then((result)=>{
-        res.status(202).json({
-            message : "User Created",
-            // result : result
+    .then((hash)=>{
+        const user = new User({
+            email : req.body.email,
+            password : hash
         });
-    })
-    .catch((err)=> {
-        res.status(501).json({
-            message : "Internal Server Error",
-            error : err
+
+        user.save()
+        .then((result)=>{
+            res.status(202).json({
+                message : "User Created",
+                // result : result
+            });    
+        })
+        .catch((err)=>{
+            res.status(501).json({
+                message : "Internal Server Error",
+                // error : err
+            });
         });
     });
 
-const login = (req, res)=> {
-    let fetchedUser =;
+    
+    
+};
+
+const login = (req, res)=>{
+    let fetchedUser;
 
     User.findOne({ email : req.body.email})
-    .then((user)=> {
-        if(user){
+    .then((user)=>{
+        if(!user){
             return res.status(401).json({
-                message : "Auth failed, email nit exists !"
+                message : "Auth failed, email not exists !"
             });
         }
-        fetchedUser = user;
+
+        fetchedUser= user;
 
         return bcrypt.compare(req.body.password, user.password);
     });
 }
-userSchema
-module.exports = {signUp}
+
+module.exports = { signUp, login}
