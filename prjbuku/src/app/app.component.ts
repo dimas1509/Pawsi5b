7fe6836b4baf3853c4c800812cfd2937eb9e3e06
olip <mdimasdimas0915@gmail.com>
2024-11-12 15:15:10 +0700

    pawsi5b  frontEnd

--- prjbuku/src/app/app.component.ts
+++ prjbuku/src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {AdminComponent} from './admin'
+
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'prjfrontend';
+  title = 'prjbuku';
 }
--- prjfrontend/src/app/app.routes.ts
+++ prjfrontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
+import { Component } from '@angular/core';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { Routes } from '@angular/router';
+
 
 export const routes: Routes = [];
