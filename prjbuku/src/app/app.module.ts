8e40f39df9b873df97446130415024a9142b364a
dimas1509 <mdimasdimas0915@gmail.com>
2024-11-13 18:43:29 +0700

    prj buku 10-11-2024

--- prjbuku/src/app/app.module.ts
+++ prjbuku/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
