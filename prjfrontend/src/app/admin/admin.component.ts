3a5f9c4e23d8dc44434294e257948084878df598
dimas1509 <mdimasdimas0915@gmail.com>
2024-11-14 10:23:39 +0700

    angular m dimas

--- prjfrontend/src/app/admin/admin.component.ts
+++ prjfrontend/src/app/admin/admin.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { DashboardComponent } from '../dashboard/dashboard.component';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-admin',
   standalone: true,
