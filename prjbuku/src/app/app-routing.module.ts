fc27f00d4b6b0881d24b348023cb60ac241136cb
dimas1509 <mdimasdimas0915@gmail.com>
2024-11-14 13:06:34 +0700

    Angular PawSI5B 14/11/2024

--- prjbuku/src/app/app-routing.module.ts
+++ prjbuku/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import path from 'node:path';
+import { AdminComponent } from './admin/admin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path : 'admin',
+  component : AdminComponent,
+  children :[
+    {
+      path: '',
+       component : DashboardComponent
+    }
+
+  ]
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
