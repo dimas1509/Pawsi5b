f3dbc710fbc9b2bb7a469e888052df3706d1acd5
dimas1509 <mdimasdimas0915@gmail.com>
2024-11-19 15:07:35 +0700

    Form Data Buku & services

--- prjbuku/src/app/buku/buku.component.ts
+++ prjbuku/src/app/buku/buku.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {NgForm} from '@angular/forms';
+import { NgForm } from '@angular/forms';
 
 
 @Component({
@@ -9,8 +9,30 @@ import {NgForm} from '@angular/forms';
 })
 export class BukuComponent {
 
-  simpanBuku = (form : NgForm){
-      console.log("Pengujian Klik")
-      console.log(form.value.judul);
-   }
+  simpanBuku(form : NgForm){
+
+    if (form.invalid){
+      console.log("Tidak Valid");
+      //alert:("Data Tidak valid");
+      return;
+    }
+    let genres: string[] =[];
+
+    if (form.value.genre1=< true){
+      genres.push("Biografi");
+    }
+    if (form.value.genre2=<true){
+      genres.push("Pendidikan");
+    }
+
+    if(form.value.genre3=<true){
+      genres.push("Lainnya");
+    }
+    console.log("Pengujian Klik")
+    console.log(form.value.judul);
+    console.log(form.value.penulis);
+    console.log(genres);
+
+    this.bukuService.addBuku
+  }
 }
---
+++ prjbuku/src/app/services/buku.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BukuService {
+  private url : string ="http://localhost:3000/bukus"; 
+
+  constructor(private http : HttpClient) { }
+
+  addBuku(judul : string, penulis : string, genres : string[]){
+    const buku : Buku={
+    _id : null,
+    judul : judul,
+    penulis : penulis,
+    genre : genres
+    }
+
+    console.log(buku);
+
+    this.http.post<{message : String}>(this.url,buku)
+    .subscribe((response)=>{
+      console.log(response.message)
+
+    });
+
+
+  }
+}
